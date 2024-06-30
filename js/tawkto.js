060b78866fecf4bdfe66cc82abb2a64a3d6f098a
dongkyukim1 <wlsntus55@gmail.com>
2024-06-30 19:50:46 +0900

    챗봇

---
+++ js/tawkto.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66812f73eaf3bd8d4d167c6d/1i1ka2mn0';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
